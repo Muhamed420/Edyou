e53ee45b13217c53fb1eb6da92293f6fdf0e1fee
Muhamed420 <180130@studierende.htl-donaustadt.at>
2021-10-24 22:17:48 +0200

    first commit

---
+++ public/js/login.js
@@ -0,0 +1,12 @@
+function validate(){
+    var username = document.getElementById("username").value
+    var password = document.getElementById("password").value
+
+    if(username =='admin' && password =='admin'){
+        alert('login succesfully')
+        window.location.replace ("views/about")
+        return false
+    }else{
+        alert('login failed')
+    }
+}
