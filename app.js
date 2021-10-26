a5831631c63c7eddff299df992a167aab1698f66
Muhamed420 <180130@studierende.htl-donaustadt.at>
2021-10-26 14:45:28 +0200

    .

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set('views', './views')
 app.set('view engine', 'ejs')
 
 app.get('', (req, res) => {
-    res.render('index')
+    res.render('home')
 })
 
 app.get('', (req, res) => {
--- public/js/nav.js
+++ public/js/nav.js
@@ -1,7 +1,7 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger')
     const nav = document.querySelector('.nav-links')
-    const navLinks = document.querySelectorAll('.nav-links l1')
+    //const navLinks = document.querySelectorAll('.nav-links l1')
 
 
     burger.addEventListener('click', () =>{
@@ -9,15 +9,14 @@ const navSlide = () => {
         nav.classList.toggle('nav-active')
 
         //Animate Links
-        navLinks.forEach((link,index) => {
-            if(link.style.animation){
-                link.style.animation = ''
-            }else{
-                link.style.animation = `navLink Fade 0.5s ease forwards ${index / 7 + 1.5}s`
-            }
-        })
+        //navLinks.forEach((link,index) => {
+        //    if(link.style.animation){
+        //       link.style.animation = ''
+        //    }else{
+        //        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`
+        //    }
+        //})
         
-
         //Burger Animation
         burger.classList.toggle('toggle')
     })
